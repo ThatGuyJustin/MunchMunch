6343840d0ba9a669b62ec740ea40ec2162c2b94d
Justin <justin@aperturebot.science>
2024-10-09 22:32:06 -0400

    Fixing broken file locations.

--- web/html/dashboard.php
+++ web/html/dashboard.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
+    <script src="js/scripts.js"></script> 
 </head>
 <body>
 
+    <?php include 'nav.php'; ?> 
 
-    <?php include 'nav.php'; ?>
-
+</body>
+</html>
 
 </body>
 </html>
