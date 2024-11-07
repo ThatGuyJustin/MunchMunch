505812bceccbc5128ebb066212b4fc94e427d84d
Justin <justin@aperturebot.science>
2024-11-07 11:58:06 -0500

    Fixing broken script

--- web/html/admin.php
+++ web/html/admin.php
@@ -129,7 +129,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <p>No tickets found.</p>
         <?php endif; ?>
     </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
